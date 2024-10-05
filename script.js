24d6e3d00b0b9a5fff2e919514a959faad259bc0
Sebastian Arce <sebastianarce25@gmail.com>
2024-10-05 19:21:21 +0100

    add form to input book parameters

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const myLibrary = [];
 
-function Book() {
-    // constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        infoMessage = `${this.title} by ${this.author}, pages ${this.pages}`;
+        if (this.read) {
+            infoMessage += ", already read.";
+        } else {
+            infoMessage += ", not read yet.";
+        }
+        return infoMessage
+    }
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    // takes users input and creates a new book object
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
